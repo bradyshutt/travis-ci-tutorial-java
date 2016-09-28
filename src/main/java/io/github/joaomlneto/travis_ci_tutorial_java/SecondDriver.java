2d54d8ce86a8919104b55edbbd8fd34b424c5b12
Brady Shutt <bshutt@csc232x05.ad.calpoly.edu>
2016-09-28 11:47:25 -0700

    second changes

---
+++ src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/SecondDriver.java
@@ -0,0 +1,17 @@
+package io.github.joaomlneto.travis_ci_tutorial_java;
+
+public class SecondDriver {
+
+	public static void main(String[] args) {
+		int num = 12;
+		
+		num *= 3;
+		num *= 7;
+		num *= 13;
+		num *= 37;
+		
+		System.out.println(num);
+				
+	}
+
+}
